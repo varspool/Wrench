5611c5752f7779365a41d8f2a25d1aaa11d3d9bf
Dominic Scheirlinck <dominic@varspool.com>
2012-07-08 06:43:09 +0000

    Added frame unit tests

--- lib/Wrench/Tests/Test.php
+++ lib/Wrench/Tests/Test.php
@@ -2,9 +2,11 @@
 
 namespace Wrench\Tests;
 
+use \PHPUnit_Framework_TestCase;
+
 /**
  * Test base class
  */
-abstract class Test extends \PHPUnit_Framework_TestCase
+abstract class Test extends PHPUnit_Framework_TestCase
 {
 }
