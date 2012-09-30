b274b5cc1c63f52c869523a9166a823f93f7796d
Dominic Scheirlinck <dominic@varspool.com>
2012-09-30 13:42:06 +0000

    Added send/receive testing to ClientTest

--- lib/Wrench/Tests/ClientTest.php
+++ lib/Wrench/Tests/ClientTest.php
@@ -2,6 +2,8 @@
 
 namespace Wrench\Tests;
 
+use Wrench\Protocol\Protocol;
+
 use Wrench\Client;
 use Wrench\Tests\Test;
 use Wrench\Socket;
@@ -104,5 +106,68 @@ class ClientTest extends Test
     public function testConstructorOriginInvalid()
     {
         $w = new Client('ws://localhost:8000', 'NOTAVALIDURI');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSendInvalidType()
+    {
+        $client = new Client('ws://localhost/test', 'http://example.org/');
+        $client->sendData('blah', 9999);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSendInvalidTypeString()
+    {
+        $client = new Client('ws://localhost/test', 'http://example.org/');
+        $client->sendData('blah', 'fooey');
+    }
+
+    public function testSend()
+    {
+        try {
+            $helper = new ServerTestHelper();
+            $helper->setUp();
+
+            /* @var $instance Wrench\Client */
+            $instance = $this->getInstance($helper->getEchoConnectionString(), 'http://www.example.com/send');
+            $instance->addRequestHeader('X-Test', 'Custom Request Header');
+
+            $this->assertFalse($instance->receive(), 'Receive before connect');
+
+            $success = $instance->connect();
+            $this->assertTrue($success, 'Client can connect to test server');
+            $this->assertTrue($instance->isConnected());
+
+            $this->assertFalse($instance->connect(), 'Double connect');
+
+            $this->assertFalse((boolean)$instance->receive(), 'No data');
+
+            $bytes = $instance->sendData('foobar', 'text');
+            $this->assertTrue($bytes >= 6, 'sent text frame');
+            sleep(1);
+
+            $bytes = $instance->sendData('baz', Protocol::TYPE_TEXT);
+            $this->assertTrue($bytes >= 3, 'sent text frame');
+            sleep(1);
+
+            $responses = $instance->receive();
+            $this->assertTrue(is_array($responses));
+            $this->assertCount(2, $responses);
+            $this->assertInstanceOf('Wrench\\Payload\\Payload', $responses[0]);
+            $this->assertInstanceOf('Wrench\\Payload\\Payload', $responses[1]);
+
+            $instance->disconnect();
+
+            $this->assertFalse($instance->isConnected());
+        } catch (\Exception $e) {
+            $helper->tearDown();
+            throw $e;
+        }
+
+        $helper->tearDown();
     }
 }
