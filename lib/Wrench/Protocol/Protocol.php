4a7a3930752df7fcd9b055bb2ee0854712782ec5
Dominic Scheirlinck <dominic@varspool.com>
2012-09-30 10:41:45 +0000

    Whitespace and formatting changes
    No other changes in this commit. Line endings to unix.

--- lib/Wrench/Protocol/Protocol.php
+++ lib/Wrench/Protocol/Protocol.php
@@ -661,7 +661,7 @@ abstract class Protocol
         $return = array();
         foreach (explode("\r\n", $headers) as $header) {
             $parts = explode(': ', $header, 2);
-            if (count($parts) == 2) {                 
+            if (count($parts) == 2) {
                  list($name, $value) = $parts;
                  $return[$name] = $value;
             }
@@ -776,90 +776,79 @@ abstract class Protocol
             throw new InvalidArgumentException('Unknown websocket scheme');
         }
     }
-    
+
     public function encode($payload, $type = 'text', $masked = true)
     {
-    	$frameHead = array();
-    	$frame = '';
-    	$payloadLength = strlen($payload);
-    
-    	switch($type)
-    	{
-    		case 'text':
-    			// first byte indicates FIN, Text-Frame (10000001):
-    			$frameHead[0] = 129;
-    			break;
-    
-    		case 'close':
-    			// first byte indicates FIN, Close Frame(10001000):
-    			$frameHead[0] = 136;
-    			break;
-    
-    		case 'ping':
-    			// first byte indicates FIN, Ping frame (10001001):
-    			$frameHead[0] = 137;
-    			break;
-    
-    		case 'pong':
-    			// first byte indicates FIN, Pong frame (10001010):
-    			$frameHead[0] = 138;
-    			break;
-    	}
-    
-    	// set mask and payload length (using 1, 3 or 9 bytes)
-    	if($payloadLength > 65535)
-    	{
-    		$payloadLengthBin = str_split(sprintf('%064b', $payloadLength), 8);
-    		$frameHead[1] = ($masked === true) ? 255 : 127;
-    		for($i = 0; $i < 8; $i++)
-    		{
-    		$frameHead[$i+2] = bindec($payloadLengthBin[$i]);
-    		}
-    		// most significant bit MUST be 0 (close connection if frame too big)
-    		if($frameHead[2] > 127)
-    		{
-    		$this->close(1004);
-    		return false;
-    		}
-    	}
-    	elseif($payloadLength > 125)
-    	{
-    	$payloadLengthBin = str_split(sprintf('%016b', $payloadLength), 8);
-    	$frameHead[1] = ($masked === true) ? 254 : 126;
-    	$frameHead[2] = bindec($payloadLengthBin[0]);
-    	$frameHead[3] = bindec($payloadLengthBin[1]);
-    	}
-    	else
-    	{
-    	$frameHead[1] = ($masked === true) ? $payloadLength + 128 : $payloadLength;
-    	}
-    
-    	// convert frame-head to string:
-    	foreach(array_keys($frameHead) as $i)
-    	{
-    	$frameHead[$i] = chr($frameHead[$i]);
-    	}
-    	if($masked === true)
-    	{
-    	// generate a random mask:
-    	$mask = array();
-    		for($i = 0; $i < 4; $i++)
-    		{
-    		$mask[$i] = chr(rand(0, 255));
-    	}
-    		
-    	$frameHead = array_merge($frameHead, $mask);
-    	}
-    	$frame = implode('', $frameHead);
-    
-    	// append payload to frame:
-    	$framePayload = array();
-    	for($i = 0; $i < $payloadLength; $i++)
-    	{
-    	$frame .= ($masked === true) ? $payload[$i] ^ $mask[$i % 4] : $payload[$i];
-    	}
-    
-    	return $frame;
+        $frameHead = array();
+        $frame = '';
+        $payloadLength = strlen($payload);
+
+        switch($type)
+        {
+            case 'text':
+                // first byte indicates FIN, Text-Frame (10000001):
+                $frameHead[0] = 129;
+                break;
+
+            case 'close':
+                // first byte indicates FIN, Close Frame(10001000):
+                $frameHead[0] = 136;
+                break;
+
+            case 'ping':
+                // first byte indicates FIN, Ping frame (10001001):
+                $frameHead[0] = 137;
+                break;
+
+            case 'pong':
+                // first byte indicates FIN, Pong frame (10001010):
+                $frameHead[0] = 138;
+                break;
+        }
+
+        // set mask and payload length (using 1, 3 or 9 bytes)
+        if($payloadLength > 65535) {
+            $payloadLengthBin = str_split(sprintf('%064b', $payloadLength), 8);
+            $frameHead[1] = ($masked === true) ? 255 : 127;
+            for($i = 0; $i < 8; $i++) {
+                $frameHead[$i+2] = bindec($payloadLengthBin[$i]);
+            }
+            // most significant bit MUST be 0 (close connection if frame too big)
+            if($frameHead[2] > 127) {
+                $this->close(1004);
+                return false;
+            }
+        } elseif($payloadLength > 125) {
+            $payloadLengthBin = str_split(sprintf('%016b', $payloadLength), 8);
+            $frameHead[1] = ($masked === true) ? 254 : 126;
+            $frameHead[2] = bindec($payloadLengthBin[0]);
+            $frameHead[3] = bindec($payloadLengthBin[1]);
+        } else {
+            $frameHead[1] = ($masked === true) ? $payloadLength + 128 : $payloadLength;
+        }
+
+        // convert frame-head to string:
+        foreach(array_keys($frameHead) as $i) {
+            $frameHead[$i] = chr($frameHead[$i]);
+        }
+
+        if($masked === true) {
+            // generate a random mask:
+            $mask = array();
+            for($i = 0; $i < 4; $i++) {
+                $mask[$i] = chr(rand(0, 255));
+            }
+            $frameHead = array_merge($frameHead, $mask);
+        }
+        $frame = implode('', $frameHead);
+
+        // append payload to frame:
+        $framePayload = array();
+        for($i = 0; $i < $payloadLength; $i++) {
+            $frame .= ($masked === true) ? $payload[$i] ^ $mask[$i % 4] : $payload[$i];
+        }
+
+        return $frame;
     }
-   
+
 }
