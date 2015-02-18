7101e39d75dd7d76c46895de4a58fef7817388d5
Dominic Scheirlinck <dominic@varspool.com>
2015-02-18 12:46:58 +0000

    Added instructions on adding security exception for cert

--- examples/server_ssl.php
+++ examples/server_ssl.php
@@ -1,6 +1,14 @@
 #!/usr/bin/env php
 <?php
 
+/*
+ * This SSL server uses a self-signed certificate. This means your browser will probably
+ * refuse to make Websocket connections to it. Chrome does *not* show this in the inspector.
+ *
+ * To add an exception, visit https://127.0.0.1:8000/ after starting this server. Then you'll
+ * get the normal security prompt.
+ */
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
