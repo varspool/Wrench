5f33a1ed7cbef170104c9859d70f65c444c0caa0
Evgeny Ukho <eukho@yandex-team.ru>
2015-12-23 21:22:54 +0300

    unblocking read from socket when it the queue is empty

--- lib/Wrench/Socket/Socket.php
+++ lib/Wrench/Socket/Socket.php
@@ -320,7 +320,13 @@ abstract class Socket extends Configurable implements Resource
 
             if ($metadata && isset($metadata['unread_bytes']) && $metadata['unread_bytes']) {
                 $continue = true;
-                $length = $metadata['unread_bytes'];
+                // it makes sense only if unread_bytes less than DEFAULT_RECEIVE_LENGTH
+                if ($length > $metadata['unread_bytes']) {
+                    $length = $metadata['unread_bytes'];
+                    // Socket is a blocking by default. When we do a blocking read from an empty
+                    // queue it will block and the server will hang. https://bugs.php.net/bug.php?id=1739
+                    stream_set_blocking($this->socket, false);
+                }
             }
         } while ($continue);
 
