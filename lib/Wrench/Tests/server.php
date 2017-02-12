f0c136df26eb53762b7e7adb03fd50c45abd26ee
Dominic Scheirlinck <dominic@varspool.com>
2017-02-12 00:06:01 +0000

    Use the Composer autoloader instead of bundling one

--- lib/Wrench/Tests/server.php
+++ lib/Wrench/Tests/server.php
@@ -1,16 +1,20 @@
 <?php
 
+$root = __DIR__ . '/../../..';
 
-require_once __DIR__ . '/../../../SplClassLoader.php';
+if (file_exists($root . '/vendor/autoload.php')) {
+    $loader = require $root . '/vendor/autoload.php';
+} elseif (file_exists($root . '/../../vendor/autoload.php')) {
+    $loader = require $root . '/../../vendor/autoload.php';
+} else {
+    throw new RuntimeException('Could not find composer autoloader; are the composer dependencies installed?');
+}
 
 if ($argc != 2 || !$argv[1] || !is_numeric($argv[1]) || (int)$argv[1] <= 1024) {
     throw new InvalidArgumentException('Invalid port number: supply as first argument');
 }
 
 $port = (int)$argv[1];
-
-$classLoader = new \SplClassLoader('Wrench', __DIR__ . '/../..');
-$classLoader->register();
 
 $server = new Wrench\Server('ws://localhost:' . $port);
 $server->registerApplication('echo', new Wrench\Application\EchoApplication());
